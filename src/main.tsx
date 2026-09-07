20801ed86901a9376744dbb4d3915f12630794c5
egangbe <egangbee@gmail.com>
2026-09-07 14:50:02 +0100

    Add AI assistant to new story page

--- src/main.tsx
+++ src/main.tsx
@@ -18,12 +18,12 @@ import App from './App.tsx'
 import AuthProvider  from './AuthProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  
     <AuthProvider>
       <App />
     </AuthProvider>
-  </StrictMode>,
   
-    <App />
+  
+    
 
 )
